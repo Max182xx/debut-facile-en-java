edd9327d6ab4e9a1ee37b153c160276633a0a2fd
Max182xx <m.garciapro34@gmail.com>
2024-11-25 17:34:44 +0100

    Java révision

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,7 +1,11 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
 
+  static  String completion = "des équipes du parking ";
+  static ParkingService parkingService = new ParkingService();
   /**
    * Ceci est la méthode Main
    *
@@ -17,11 +21,15 @@ public class ParkingBot {
    * Cette méthode permet de dire Hello
    */
   private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue ";
+    String welcomeSentence = "Hello, recevez la bienvenue " + completion;
     System.out.println(welcomeSentence);
+    String parkService = parkingService.parking;
+    String washinService = parkingService.washing;
+
+    System.out.println("Nous proposons les services suivant : "+ parkService + "et " + washinService);
   }
 
   private static void sayBye() {
-    System.out.println("Recevez les aurevoirs");
+    System.out.println("Recevez les aurevoirs " + completion);
   }
 }

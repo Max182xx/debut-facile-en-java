328102025217ca55c26c9930f327fac09a1be022
Max182xx <m.garciapro34@gmail.com>
2024-11-28 14:13:13 +0100

    Les listes

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,7 +1,13 @@
 package com.zerofiltre.parkingbot;
 
+import java.security.SecureRandom;
+
 public class ParkingBot {
 
+  static String[] customers =
+      {"Leye", "Ali", "Réné", "Booba", "Léa", "Hamza", "Philippe", "Zineb", "Jules", "Mukul", "Will"};
+
+  static int parkingSize = new SecureRandom().nextInt(10);
 
   /**
    * Ceci est la méthode Main
@@ -9,8 +15,17 @@ public class ParkingBot {
    * @param args : Tableau de données entrées lors du lancement de l'application
    */
   public static void main(String[] args) {
+
   }
 
 
+  /**
+   *
+   * @param customer Le client à qui dire Hello
+   */
+  private static void sayHello(String customer) {
+    String welcomeSentence = "Hello " + customer;
+    System.out.println(welcomeSentence);
+  }
 
 }
